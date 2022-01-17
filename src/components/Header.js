734d7f1b09e09ff737da2507a410bf659bcf51b9
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-01-17 01:41:13 -0500

    implemented fake data

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ function Header() {
   return (
     <div>
       <h1>i am the header</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit impedit eos ipsa culpa perspiciatis corrupti accusantium voluptatibus fugiat, tenetur repudiandae blanditiis, nostrum necessitatibus aliquid doloremque corporis reiciendis amet itaque aspernatur!
+      </h2>
     </div>
   )
 }
