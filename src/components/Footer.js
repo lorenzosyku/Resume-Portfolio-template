9b5470ab98f1326c53b7c98b96f187c45ef4c7f7
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-01 21:25:48 -0500

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
-function Footer() {
+function Footer({data}) {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Made by{" Lorenzo Syku"}
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
