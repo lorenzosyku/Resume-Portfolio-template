5a0bf0384a594996e5d58756204108d1607cb680
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-02 00:55:54 -0500

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import TypeWriter from "react-typewriter";
+
 function Header({data}) {
   if (data) {
     var name = data.name;
@@ -15,10 +17,75 @@ function Header({data}) {
     });
   }
   return (
-    <div>
-      
-    </div>
-  )
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
+
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="row banner">
+        <div >
+          <div className="banner-text">
+            <h1 className="responsive-headline">
+              <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
+            </h1>
+            <h3>
+              Based in {city}. <span>{occupation}</span>. {description}.
+            </h3>
+            <hr />
+            <div>
+              <ul className="social">{networks}</ul>
+            </div>
+            
+          </div>
+        </div>
+        
+      </div>
+
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle"></i>
+        </a>
+      </p>
+    </header>
+  );
 }
 
 export default Header
