516683746c7fd19012743f67ab6a2246427ec723
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-02-09 00:34:03 -0500

    formated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,7 +31,7 @@ function Contact({ data }) {
     <section className="max-w-3xl mx-auto p-5 m-5 bg-prussian-blue-50 text-gray-300" id="contact">
       <div className="mb-5">
         <div>
-          <p className="font-bold">{contactMessage}</p>
+          <p className="font-semibold text-2xl">{contactMessage}</p>
         </div>
       </div>
 
@@ -42,7 +42,7 @@ function Contact({ data }) {
             onSubmit={submitForm}
           >
             <fieldset>
-              <div>
+              <div className="mb-5">
                 <label htmlFor="contactName">
                   Name <span className="required">*</span>
                 </label>
@@ -58,7 +58,7 @@ function Contact({ data }) {
                 />
               </div>
 
-              <div>
+              <div className="mb-5">
                 <label htmlFor="contactEmail">
                   Email <span className="required">*</span>
                 </label>
@@ -74,7 +74,7 @@ function Contact({ data }) {
                 />
               </div>
 
-              <div>
+              <div className="mb-5">
                 <label htmlFor="contactSubject">Subject</label>
                 <input
                   className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-cyan-400 outline-none focus:ring"
@@ -88,7 +88,7 @@ function Contact({ data }) {
                 />
               </div>
 
-              <div>
+              <div className="mb-5">
                 <label htmlFor="contactMessage">
                   Message <span className="required">*</span>
                 </label>
